15c17ec854cf7d892967250bc4d5c10fc0f0276c
Luis Filipe S.Lima <luisphilip90@gmail.com>
2022-12-06 00:08:25 -0300

    Alteração base url de chamada da api do auth

--- js/enums/DominioEnum.js
+++ js/enums/DominioEnum.js
@@ -1,11 +1,11 @@
-// const DOMINIO = Object.freeze({
-// 	DESENVOLVIMENTO: "https://skauth-dev.skymed.app.br/api",
-// 	PRODUCAO: "https://skauth.skymed.app.br/api"
-// })
-
 const DOMINIO = Object.freeze({
-	DESENVOLVIMENTO: "http://localhost:8083/api",
-	PRODUCAO: "http://localhost:8083/api"
+	DESENVOLVIMENTO: "https://skauth-dev.skymed.app.br/api",
+	PRODUCAO: "https://skauth.skymed.app.br/api"
 })
+
+// const DOMINIO = Object.freeze({
+// 	DESENVOLVIMENTO: "http://localhost:8083/api",
+// 	PRODUCAO: "http://localhost:8083/api"
+// })
 
 export { DOMINIO }
