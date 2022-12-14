bae8195b081bd441662e53594b7b48f382b504ce
Luis Filipe <luisphilip90@gmail.com>
2022-12-14 16:25:20 -0300

    coreção do atrado do tempo de servidor

--- js/dtos/SenhaDto.js
+++ js/dtos/SenhaDto.js
@@ -1,7 +1,8 @@
 class SenhaDto{
+    FATOR_DE_CORRECAO_HORA = 60*1000;
     constructor(senha){
         this.password = senha;
-        this.data = new Date(Date.now()).toISOString();
+        this.data = new Date(Date.now()-FATOR_DE_CORRECAO_HORA).toISOString();
     }
 
     serializar(){
